553b0910615a9588592e3a9bea903072a8e0070c
Tomasz Adamski <tadamski@redhat.com>
2018-01-04 13:29:05 +0100

    Working app

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,20 +1,21 @@
 
 <html>
   <head>
-    <title>Booker</title>
+    <title>Cloud petstore</title>
     <link rel="stylesheet" href="/css/app.css"/>
 		<title>React!	React!	React!</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/JSXTransformer.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react-with-addons.js"></script>
 		<script	src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
         <script   src="https://cdnjs.cloudflare.com/ajax/libs/react-router/0.13.3/ReactRouter.js"></script>
+        <script src="http://keycloak-petstore.192.168.42.229.nip.io/auth/js/keycloak.js"></script>
 
   </head>
 
   <body>
 		<div id="container">
 		</div>
-		<script	type="text/babel" src="js/app.js">
+		 <script	type="text/babel" src="js/app.js">
 
 		</script>
 </body>
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -4,13 +4,69 @@ var DefaultRoute = Router.DefaultRoute;
 var Link = Router.Link;
 var Route = Router.Route;
 
+var Item = React.createClass({
+  render: function() {
+    var item = this.props.item
+    return (
+      <div className="pet">
+      <h3>{item.name}</h3>
+      {item.description}<br/>
+      price: {item.price}<br/>
+      <button>Add to cart</button>
+      </div>
+    );
+  }
+});
+
 var Catalog = React.createClass({
-    render: function() {
-        return (
-            <div id = "header" className = "row" >
-            <h1>STORE</h1>
-            </div>
-        );
+  render: function() {
+    console.log("RENDER CHOCIAZ :(")
+    console.log(this.state.items)
+    this.state.items.map(function(e){
+        console.log(e.name);
+    })
+    return (
+      <div>
+        <h1>Pets</h1>
+        {
+          this.state.items.map(function(e) {
+            return (
+              <Item key={e.name} item={e}/>
+            );
+          })
+        }
+      </div>
+    )
+  },
+
+  getInitialState:	function()	{
+    return	{
+        items:	[]
+    };
+  },
+
+
+    componentDidMount: function() {
+    console.log("MONTUJE?");
+       fetch('http://petstore-service-petstore.192.168.42.229.nip.io/pet', {method: 'get'})
+       .then(
+            d => {
+                console.log(d)
+                return d.json();
+
+        })
+       .then(d => {
+           console.log("USTAWIAM STAN")
+           console.log(d)
+           this.setState({items: d})
+           console.log(this.state.items)
+           console.log("USTAWILEM STAN")
+
+       })
+       .catch(function(err) {
+            console.log("PIES")
+           console.log(err.stack)
+       });
     }
 });
 
@@ -24,6 +80,18 @@ var Cart = React.createClass({
     }
 });
 
+var Logger = React.createClass({
+  onClick: function() {
+    console.log("bede logowal");
+    keycloak.login();
+  },
+  render: function() {
+    return (
+      <a href="" onClick={this.onClick}>Login</a>
+    )
+  }
+});
+
 var Header = React.createClass({
     render: function() {
         return (
@@ -33,6 +101,7 @@ var Header = React.createClass({
                     <li> <Link to = "catalog" > Catalog < /Link></li>
                     <li> <Link to = "cart" > Cart < /Link></li>
                 </ul>
+                <Logger/>
             </div >
         );
     }
